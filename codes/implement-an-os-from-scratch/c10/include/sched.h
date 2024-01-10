bdf002cddcc02a7e18f8f9ef06179a2164ca2b86
Relph1119 <huruifeng1202@163.com>
2024-01-10 21:39:51 +0800

    编写第9章学习笔记

--- codes/implement-an-os-from-scratch/c10/include/sched.h
+++ codes/implement-an-os-from-scratch/c10/include/sched.h
@@ -8,7 +8,7 @@ struct task {
     unsigned long rip;     // 任务调度切换时，指令的地址
     unsigned long rsp0;    // 任务调度切换时，内核栈的栈顶
     unsigned long kstack;  // 内核栈的栈底，用于在内核时，任务状态段中特权级为0的栈指针指向当前任务的内核栈栈底
-    unsigned long pml4;    // 根页表的地址，用于更新寄存器CR3指向当前任务的页表
+    unsigned long pml4;    // 根页表的物理地址，用于更新寄存器CR3指向当前任务的页表
 
     // 支撑任务链的链表
     struct task* next;     
