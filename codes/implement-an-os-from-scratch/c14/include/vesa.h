316c48c02ff1d03f0b71aa4d15a10f50bab041f1
Relph1119 <huruifeng1202@163.com>
2024-01-11 23:51:24 +0800

    编写第14章学习笔记

--- codes/implement-an-os-from-scratch/c14/include/vesa.h
+++ codes/implement-an-os-from-scratch/c14/include/vesa.h
@@ -18,10 +18,10 @@ struct vesa_mode_info {
 
 // 精简的结构体，用于应用程序获取模式信息
 struct mode_info {
-    uint32_t fbbase;
-    uint16_t hres;
-    uint16_t vres;
-    uint8_t bpp;
+    uint32_t fbbase; // framebuffer占据的地址空间的起始地址
+    uint16_t hres;   // 以像素为单位的水平分辨率
+    uint16_t vres;   // 以像素为单位的垂直分辨率
+    uint8_t bpp;     // 每个像素使用的字节数
 };
 
 void vesa_init();
